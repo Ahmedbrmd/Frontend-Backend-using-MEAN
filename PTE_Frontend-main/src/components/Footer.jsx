c97adb143831967308a01af756ec420bb19f5eb4
Ahmedbrmd <110302550+Ahmedbrmd@users.noreply.github.com>
2023-06-06 14:40:28 +0100

    Delete PTE_Frontend-main directory

--- PTE_Frontend-main/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => (
-  <div className="mt-24">
-    <p className="dark:text-gray-200 text-gray-700 text-center m-20">
-      © 2022 All rights reserved by Prologic
-    </p>
-  </div>
-);
-
-export default Footer;
